5030d55fa60e66eea37111d54d6e1ae1e758759b
Matea Cvetanov <cvetanov.matea@gmail.com>
2024-04-09 16:57:02 +0200

    Refactor

--- CompareJsonFiles/Program.cs
+++ CompareJsonFiles/Program.cs
@@ -1,57 +1,62 @@
 ﻿using Newtonsoft.Json;
 using System.Diagnostics;
-using System.IO;
 
-namespace CompareJsonFiles
+namespace CompareJsonFiles;
+
+public class Program
 {
-    public class Program
+    private static void Main()
     {
-        static void Main(string[] args)
+        var watch = Stopwatch.StartNew();
+        watch.Start();
+
+        Console.ForegroundColor = ConsoleColor.DarkYellow;
+        Console.WriteLine("Process started...");
+
+        var jsonToEditFrom = DeserializeAndReadObject("../../../Unprocessed/TestJson1.json");
+        var jsonToEdit = DeserializeAndReadObject("../../../Unprocessed/TestJson2.json");
+
+        CompareAndTransferValues(jsonToEditFrom, jsonToEdit);
+
+        SerializeAndSaveObject(jsonToEdit);
+
+        watch.Stop();
+
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine($"Process finished in: {watch.Elapsed}");
+    }
+
+    private static dynamic DeserializeAndReadObject(string path)
+    {
+        var jsonString = File.ReadAllText(path);
+        var DesObject = JsonConvert.DeserializeObject<dynamic>(jsonString);
+
+        return DesObject;
+    }
+
+    private static void SerializeAndSaveObject(dynamic? dObject)
+    {
+        var processedObject = JsonConvert.SerializeObject(dObject, Formatting.Indented);
+        File.WriteAllText("../../../Processed/Final.json", processedObject);
+    }
+
+    private static void CompareAndTransferValues(dynamic source, dynamic destination)
+    {
+        var sourceDictionary = source.ToObject<Dictionary<string, string>>();
+
+        var wordsTranslated = 0;
+
+        foreach (var kvp in destination)
         {
-            var watch = Stopwatch.StartNew();
-            watch.Start();
-            Console.WriteLine("Process started...");
-
-            var DObjectToEdit = ReadAndDeserializeObject(@"./SharedResource.Shq.json");
-            var DObjectToEditFrom = ReadAndDeserializeObject(@"./AFK_En.json");
-
-            var wordsTranslated = 0;
-
-            foreach (var dataToEdit in DObjectToEdit)
+            string key = kvp.Name.ToLower();
+            if (sourceDictionary.TryGetValue(key, out string value))
             {
-                //string word = dataToEdit.Name;
-                //if (word.Contains("_original"))
-                //{
-                //    word = word.Replace("_original", "");
-                //}
-
-                foreach (var dataToInsert in DObjectToEditFrom)
-                {
-                    if (dataToEdit.Name.ToLower() == dataToInsert.Name.ToLower())
-                    {
-                        if (dataToInsert.Value != null && dataToEdit.Value != null)
-                        {
-                            dataToEdit.Value = dataToInsert.Value;
-                        }
-                        Console.Write("\r{0} words translated", wordsTranslated);
-                        wordsTranslated++;
-                        continue;
-                    }
-                }
+                kvp.Value = value;
+                wordsTranslated++;
             }
-
-            var SObject = JsonConvert.SerializeObject(DObjectToEdit, Formatting.Indented);
-            File.WriteAllText(@"./SharedResource.Shq.json", SObject);
-            watch.Stop();
-            Console.WriteLine($"Process finished in: {watch.Elapsed}");
         }
 
-        public static dynamic ReadAndDeserializeObject(string path)
-        {
-            string jsonString = File.ReadAllText(path);
-            var DesObject = JsonConvert.DeserializeObject<dynamic>(jsonString);
-
-            return DesObject;
-        }
+        Console.ForegroundColor = ConsoleColor.Cyan;
+        Console.Write("Number of words translated: {0} \n", wordsTranslated);
     }
 }
